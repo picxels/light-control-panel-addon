c2d535b39cd2dc8494caba2881654981d28244a8
Sergiu Tavitian <picxels@gmail.com>
2023-03-25 16:23:23 -0400

    Update LightControlPanel.py
    Added a Label so one can move the frame around and changed the toggle buttons

--- LightControlPanel.py
+++ LightControlPanel.py
@@ -28,7 +28,7 @@ import bpy
 bl_info = {
     "name": "Light Control Panel",
     "author": "Sergiu Tavitian",
-    "version": (1, 2),
+    "version": (1, 3),
     "blender": (3, 4, 1),
     "location": "View3D > Right-Click(ObjectContextMenu)",
     "description": "Aggregates all lights in the scene and opens a menu with controls",
@@ -37,69 +37,61 @@ bl_info = {
     "category": "3D View",
 }
 
-class LightControlPanelSettings(bpy.types.PropertyGroup):
-    use_heavy_version: bpy.props.BoolProperty(name="Heavy Version", default=False)
-
 class LIGHTCONTROL_OT_show_popup(bpy.types.Operator):
     bl_idname = "lightcontrol.show_popup"
     bl_label = "Light Control Panel"
     bl_options = {'REGISTER'}
 
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'OBJECT'
+
     def execute(self, context):
-        return context.window_manager.invoke_popup(self)
+        return context.window_manager.invoke_popup(self, width=600)
 
     def draw(self, context):
         layout = self.layout
+        layout.label(text="Light Control Panel")  # Add an empty label with extra padding to increase the draggable area.
+        layout.row().separator(factor=2)  # Add more padding.
+
         scene = context.scene
-        addon_settings = scene.light_control_panel_settings
+        layout.prop(scene, "light_control_mode", expand=True)
 
-        layout.prop(addon_settings, "use_heavy_version")
-
-        lights = [obj for obj in bpy.context.scene.objects if obj.type == 'LIGHT']
+        lights = [obj for obj in scene.objects if obj.type == 'LIGHT']
 
         for light in lights:
             box = layout.box()
             col = box.column()
 
-            col.label(text=light.name)
-            col.prop(light.data, "type", text="")
-            col.prop(light.data, "energy")
-            col.prop(light.data, "color", text="")
-            col.prop(light.data, "shadow_soft_size", text="Size")
-
-            if addon_settings.use_heavy_version:
-                col.prop(light.data, "use_shadow")
-                col.prop(light.data, "specular_factor")
-
-                if light.data.type == 'SPOT':
-                    col.prop(light.data, "spot_size", text="Spot Size")
-                    col.prop(light.data, "spot_blend", text="Spot Blend")
-                    col.prop(light.data, "show_cone")
-
-                if light.data.type == 'AREA':
-                    col.prop(light.data, "shape", text="Shape")
-                    if light.data.shape == 'RECTANGLE':
-                        col.prop(light.data, "size", text="Size X")
-                        col.prop(light.data, "size_y", text="Size Y")
-                    else:
-                        col.prop(light.data, "size", text="Size")
+            if scene.light_control_mode == 'LIGHT':
+                col.label(text=light.name)
+                col.prop(light.data, "type", text="")
+                col.prop(light.data, "energy")
+                col.prop(light.data, "color", text="")
+                col.prop(light.data, "shadow_soft_size", text="Size")
+            else:
+                col.label(text=light.name)
+                col.prop(light.data, "type", text="")
+                col.prop(light.data, "energy")
+                col.prop(light.data, "color", text="")
+                col.prop(light.data, "shadow_soft_size", text="Size")
+                col.prop(light.data, "specular_factor", text="Specular")
+                col.prop(light.data, "shadow_buffer_clip_start", text="Clip Start")
+                col.prop(light.data, "shadow_buffer_clip_end", text="Clip End")
 
 def menu_func(self, context):
     self.layout.operator("lightcontrol.show_popup")
 
 def register():
-    bpy.utils.register_class(LightControlPanelSettings)
-    bpy.types.Scene.light_control_panel_settings = bpy.props.PointerProperty(type=LightControlPanelSettings)
-
     bpy.utils.register_class(LIGHTCONTROL_OT_show_popup)
     bpy.types.VIEW3D_MT_object_context_menu.append(menu_func)
+    bpy.types.Scene.light_control_mode = bpy.props.EnumProperty(items=[('LIGHT', 'Light', 'Light mode'), ('HEAVY', 'Heavy', 'Heavy mode')], name="Light Control Mode", default='LIGHT')
 
 def unregister():
-    bpy.utils.unregister_class(LightControlPanelSettings)
-    del bpy.types.Scene.light_control_panel_settings
-
     bpy.utils.unregister_class(LIGHTCONTROL_OT_show_popup)
     bpy.types.VIEW3D_MT_object_context_menu.remove(menu_func)
+    del bpy.types.Scene.light_control_mode
 
 if __name__ == "__main__":
     register()
+
